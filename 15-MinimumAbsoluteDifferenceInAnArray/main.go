0e882c92e7490ec631602b4cebecd396ee202963
mehrdad <jafari.ceai@gmail.com>
2024-05-25 01:20:49 +0330

    solve Minimum Absolute Difference In An Array

---
+++ 15-MinimumAbsoluteDifferenceInAnArray/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'minimumAbsoluteDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func minimumAbsoluteDifference(arr []int32) int32 {
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+	min := int32(1000000)
+	for i := 0; i < len(arr)-1; i++ {
+		v := arr[i] - arr[i+1]
+		if v < 0 {
+			v *= -1
+		}
+		if v < min {
+			min = v
+		}
+
+	}
+	return min
+}
+
+func main() {
+	osFile, _ := os.Open("TestCase1.txt")
+	reader := bufio.NewReader(osFile)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := minimumAbsoluteDifference(arr)
+
+	fmt.Println(result)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
