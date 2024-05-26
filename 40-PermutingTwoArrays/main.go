24cf5981b74271d76ea1dfd635a84a54f1be303b
mehrdad <jafari.ceai@gmail.com>
2024-05-27 01:18:26 +0330

    remove comment

--- 40-PermutingTwoArrays/main.go
+++ 40-PermutingTwoArrays/main.go
@@ -10,16 +10,6 @@ import (
 	"strings"
 )
 
-/*
- * Complete the 'twoArrays' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts following parameters:
- *  1. INTEGER k
- *  2. INTEGER_ARRAY A
- *  3. INTEGER_ARRAY B
- */
-
 func twoArrays(k int32, A []int32, B []int32) string {
 	sort.Slice(A, func(i, j int) bool {
 		return A[i] < A[j]
